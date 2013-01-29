2909769a01a343407ec1a1d5ff9f95cf2b4046bc
Julian Churchill <chewielouie03@gmail.com>
2013-01-29 19:46:45 +0000

    Added Exit class

---
+++ AndroidFree/src/main/java/com/chewielouie/textadventure/Exit.java
@@ -0,0 +1,14 @@
+package com.chewielouie.textadventure;
+
+public class Exit {
+    private String label;
+
+    public Exit( String label ) {
+        this.label = label;
+    }
+
+    public String label() {
+        return this.label;
+    }
+}
+
