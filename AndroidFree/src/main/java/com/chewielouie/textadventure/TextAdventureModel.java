49f628699eaf2724d4e492f4f9d969028a70b199
Julian Churchill <chewielouie03@gmail.com>
2013-02-08 20:43:07 +0000

    Pushed up addlocation interface to TextAdventureModel

--- AndroidFree/src/main/java/com/chewielouie/textadventure/TextAdventureModel.java
+++ AndroidFree/src/main/java/com/chewielouie/textadventure/TextAdventureModel.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public interface TextAdventureModel {
     public String currentLocationDescription();
+    public void addLocation( ModelLocation location );
     public List<Exit> currentLocationExits();
     public void moveThroughExit( Exit exit );
     public List<Item> inventoryItems();
