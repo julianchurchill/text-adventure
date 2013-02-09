51443446af756394fefa3d324bb2497741e36ee7
Julian Churchill <chewielouie03@gmail.com>
2013-02-09 16:18:27 +0000

    Expanded model populator to handle multiple locations

--- AndroidFree/src/main/java/com/chewielouie/textadventure/PlainTextModelPopulator.java
+++ AndroidFree/src/main/java/com/chewielouie/textadventure/PlainTextModelPopulator.java
@@ -1,10 +1,37 @@
 package com.chewielouie.textadventure;
 
 public class PlainTextModelPopulator {
+    private final String locationNameTag = "location_name:";
+    private int currentIndex = 0;
+    private TextAdventureModel model;
+    private ModelLocationFactory locationFactory;
+    private String content;
+
     public PlainTextModelPopulator( TextAdventureModel model,
                                     ModelLocationFactory locationFactory,
                                     String content ) {
-        model.addLocation( locationFactory.create( "name" ) );
+        this.model = model;
+        this.locationFactory = locationFactory;
+        this.content = content;
+        while( moreContentToParse() )
+            model.addLocation( locationFactory.create( extractName() ) );
+    }
+
+    private boolean moreContentToParse() {
+        return currentIndex != -1 && currentIndex < content.length();
+    }
+
+    private String extractName() {
+        currentIndex = content.indexOf( locationNameTag, currentIndex );
+        int newlineIndex = content.indexOf( "\n", currentIndex );
+        if( newlineIndex == -1 )
+            newlineIndex = content.length();
+        String name = "";
+        if( currentIndex != -1 ) {
+            name = content.substring( currentIndex+locationNameTag.length(), newlineIndex );
+            currentIndex = currentIndex + 1;
+        }
+        return name;
     }
 }
 
--- AndroidFree/src/test/java/com/chewielouie/textadventure/PlainTextModelPopulatorTests.java
+++ AndroidFree/src/test/java/com/chewielouie/textadventure/PlainTextModelPopulatorTests.java
@@ -31,5 +31,30 @@ public class PlainTextModelPopulatorTests {
         new PlainTextModelPopulator( model, locationFactory,
                                      "location_name:name" );
     }
+
+    @Test
+    public void multiple_locations_are_added_by_name_to_model() {
+        final TextAdventureModel model = mockery.mock( TextAdventureModel.class );
+        final ModelLocation location1 = mockery.mock( ModelLocation.class, "l1" );
+        final ModelLocation location2 = mockery.mock( ModelLocation.class, "l2" );
+        final ModelLocationFactory locationFactory =
+            mockery.mock( ModelLocationFactory.class );
+
+        mockery.checking( new Expectations() {{
+            allowing( locationFactory ).create( "name1" );
+            will( returnValue( location1 ) );
+            allowing( locationFactory ).create( "name2" );
+            will( returnValue( location2 ) );
+            ignoring( locationFactory );
+            ignoring( location1 );
+            ignoring( location2 );
+            oneOf( model ).addLocation( location1 );
+            oneOf( model ).addLocation( location2 );
+            ignoring( model );
+        }});
+
+        new PlainTextModelPopulator( model, locationFactory,
+                                     "location_name:name1\nlocation_name:name2" );
+    }
 }
 
