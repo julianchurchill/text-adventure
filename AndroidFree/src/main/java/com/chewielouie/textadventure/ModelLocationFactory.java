04c742dcf6fc947917ddb51d71b68490f029af68
Julian Churchill <chewielouie03@gmail.com>
2013-02-12 20:35:02 +0000

    Removed unused methods gtom ModelLocationfactory

--- AndroidFree/src/main/java/com/chewielouie/textadventure/ModelLocationFactory.java
+++ AndroidFree/src/main/java/com/chewielouie/textadventure/ModelLocationFactory.java
@@ -2,8 +2,6 @@ package com.chewielouie.textadventure;
 
 public interface ModelLocationFactory {
     public ModelLocation create();
-    public ModelLocation create( String name );
-    public ModelLocation create( String name, String description );
 }
 
 
