b750781a78a9dbe72ed791982f2d6d96d1c22972
Julian Churchill <chewielouie03@gmail.com>
2013-02-14 16:36:42 +0000

    Removed ItemFactory create method with lots of parameters

--- AndroidFree/src/main/java/com/chewielouie/textadventure/ItemFactory.java
+++ AndroidFree/src/main/java/com/chewielouie/textadventure/ItemFactory.java
@@ -2,9 +2,5 @@ package com.chewielouie.textadventure;
 
 public interface ItemFactory {
     public Item create();
-    public Item create( String name,
-                String description,
-                String countableNounPrefix,
-                String midSentenceCasedName );
 }
 
