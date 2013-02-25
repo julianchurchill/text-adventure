79ee29bfbe8504113fe74aaef8b743b270b1b935
Julian Churchill <chewielouie03@gmail.com>
2013-02-25 20:07:31 +0000

    Moved ModelLocationDeserialiser into its own file

--- AndroidFree/src/main/java/com/chewielouie/textadventure/Location.java
+++ AndroidFree/src/main/java/com/chewielouie/textadventure/Location.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.chewielouie.textadventure.action.Action;
 import com.chewielouie.textadventure.action.TakeAnItem;
 import com.chewielouie.textadventure.action.ExamineAnItem;
+import com.chewielouie.textadventure.serialisation.ModelLocationDeserialiser;
 
 public class Location implements ModelLocation {
     private String id;
@@ -13,8 +14,7 @@ public class Location implements ModelLocation {
     private List<Item> items = new ArrayList<Item>();
     private UserInventory inventory;
     private ItemFactory itemFactory = null;
-    private ModelLocationDeserialiser deserialiser =
-        new ModelLocationDeserialiser( this );
+    private ModelLocationDeserialiser deserialiser;
 
     public Location( String locationId, String description,
             UserInventory inventory, ItemFactory itemFactory ) {
@@ -22,6 +22,7 @@ public class Location implements ModelLocation {
         this.description = description;
         this.inventory = inventory;
         this.itemFactory = itemFactory;
+        deserialiser = new ModelLocationDeserialiser( this, itemFactory );
     }
 
     public UserInventory inventory() {
@@ -126,137 +127,6 @@ public class Location implements ModelLocation {
 
     public void deserialise( String content ) {
         deserialiser.parse( content );
-    }
-
-    class ModelLocationDeserialiser {
-        private final String locationIDTag = "location id:";
-        private final String locationDescriptionTag = "location description:";
-        private final String exitLabelTag = "exit label:";
-        private final String exitDestinationTag = "exit destination:";
-        private final String exitDirectionHintTag = "exit direction hint:";
-        private final String exitIsNotVisibleTag = "exit is not visible:";
-        private final String exitIDTag = "exit id:";
-        private final String itemTag = "ITEM\n";
-        private String content;
-        private int startOfLastFoundTag = -1;
-        private ModelLocation location;
-
-        public ModelLocationDeserialiser( ModelLocation location ) {
-            this.location = location;
-        }
-
-        public void parse( String content ) {
-            this.content = content;
-            startOfLastFoundTag = -1;
-
-            location.setId( extractNewlineDelimitedValueFor( locationIDTag ) );
-            deserialiseDescription();
-            deserialiseExits();
-            deserialiseItems();
-        }
-
-        private String extractNewlineDelimitedValueFor( String tag ) {
-            int startOfTag = content.indexOf( tag, startOfLastFoundTag + 1 );
-            if( startOfTag == -1 )
-                return "";
-            startOfLastFoundTag = startOfTag;
-            int endOfTag = content.indexOf( "\n", startOfTag );
-            if( endOfTag == -1 )
-                endOfTag = content.length();
-            return content.substring( startOfTag + tag.length(), endOfTag );
-        }
-
-        private void deserialiseDescription() {
-            int startOfDescription = content.indexOf( locationDescriptionTag );
-            if( startOfDescription != -1 )
-                location.setLocationDescription( content.substring(
-                        startOfDescription + locationDescriptionTag.length(),
-                        findEndOfDescription() ) );
-        }
-
-        private int findEndOfDescription() {
-            int endOfDescription = content.indexOf( exitLabelTag );
-            if( endOfDescription == -1 ) {
-                endOfDescription = content.indexOf( itemTag );
-                if( endOfDescription == -1 )
-                    endOfDescription = content.length();
-            }
-            return endOfDescription;
-        }
-
-        private void deserialiseExits() {
-            String exitLabel;
-            while( (exitLabel=extractNewlineDelimitedValueFor( exitLabelTag )) != "" ) {
-                LocationExit exit =  new LocationExit(
-                    exitLabel,
-                    extractNewlineDelimitedValueFor( exitDestinationTag ),
-                    stringToDirectionHint(
-                        extractNewlineDelimitedValueFor( exitDirectionHintTag ) ) );
-
-                if( exitNotVisibleIsSpecifiedDiscardIt() )
-                    exit.setInvisible();
-                exit.setID( extractExitID() );
-                location.addExit( exit );
-            }
-        }
-
-        private boolean exitNotVisibleIsSpecifiedDiscardIt() {
-            int startOfTag = content.indexOf( exitIsNotVisibleTag, startOfLastFoundTag + 1 );
-            if( indexIsInCurrentExit( startOfTag ) ) {
-                extractNewlineDelimitedValueFor( exitIsNotVisibleTag );
-                return true;
-            }
-            return false;
-        }
-
-        private String extractExitID() {
-            int startOfTag = content.indexOf( exitIDTag, startOfLastFoundTag + 1 );
-            if( indexIsInCurrentExit( startOfTag ) )
-                return extractNewlineDelimitedValueFor( exitIDTag );
-            return "";
-        }
-
-        private boolean indexIsInCurrentExit( int index ) {
-            int nextExit = content.indexOf( exitLabelTag, startOfLastFoundTag + 1 );
-            if( index != -1 && ( nextExit == -1 || nextExit > index ) )
-                return true;
-            return false;
-        }
-
-        private void deserialiseItems() {
-            if( itemFactory != null ) {
-                while( itemAvailable() ) {
-                    int startOfItem = content.indexOf( itemTag, startOfLastFoundTag+1 );
-                    int endOfItem = content.indexOf( itemTag, startOfItem+1 );
-                    if( endOfItem == -1 ) {
-                        endOfItem = content.indexOf( locationIDTag, startOfItem+1 );
-                        if( endOfItem == -1 )
-                            endOfItem = content.length();
-                    }
-                    Item item = itemFactory.create();
-                    item.deserialise(
-                      content.substring( startOfItem + itemTag.length(), endOfItem ) );
-                    location.addItem( item );
-                    startOfLastFoundTag = startOfItem;
-                }
-            }
-        }
-
-        private boolean itemAvailable() {
-            return content.indexOf( itemTag, startOfLastFoundTag+1 ) != -1;
-        }
-
-        private Exit.DirectionHint stringToDirectionHint( String hint ) {
-            if( hint.equals( "North" ) )
-                return Exit.DirectionHint.North;
-            if( hint.equals( "South" ) )
-                return Exit.DirectionHint.South;
-            if( hint.equals( "East" ) )
-                return Exit.DirectionHint.East;
-            if( hint.equals( "West" ) )
-                return Exit.DirectionHint.West;
-            return Exit.DirectionHint.DontCare;
-        }
     }
 }
 
---
+++ AndroidFree/src/main/java/com/chewielouie/textadventure/serialisation/ModelLocationDeserialiser.java
@@ -0,0 +1,142 @@
+package com.chewielouie.textadventure.serialisation;
+
+import com.chewielouie.textadventure.Exit;
+import com.chewielouie.textadventure.Item;
+import com.chewielouie.textadventure.ItemFactory;
+import com.chewielouie.textadventure.ModelLocation;
+import com.chewielouie.textadventure.LocationExit;
+
+public class ModelLocationDeserialiser {
+    private final String locationIDTag = "location id:";
+    private final String locationDescriptionTag = "location description:";
+    private final String exitLabelTag = "exit label:";
+    private final String exitDestinationTag = "exit destination:";
+    private final String exitDirectionHintTag = "exit direction hint:";
+    private final String exitIsNotVisibleTag = "exit is not visible:";
+    private final String exitIDTag = "exit id:";
+    private final String itemTag = "ITEM\n";
+    private String content;
+    private int startOfLastFoundTag = -1;
+    private ModelLocation location;
+    private ItemFactory itemFactory;
+
+    public ModelLocationDeserialiser( ModelLocation location,
+           ItemFactory itemFactory ) {
+        this.location = location;
+        this.itemFactory = itemFactory;
+    }
+
+    public void parse( String content ) {
+        this.content = content;
+        startOfLastFoundTag = -1;
+
+        location.setId( extractNewlineDelimitedValueFor( locationIDTag ) );
+        deserialiseDescription();
+        deserialiseExits();
+        deserialiseItems();
+    }
+
+    private String extractNewlineDelimitedValueFor( String tag ) {
+        int startOfTag = content.indexOf( tag, startOfLastFoundTag + 1 );
+        if( startOfTag == -1 )
+            return "";
+        startOfLastFoundTag = startOfTag;
+        int endOfTag = content.indexOf( "\n", startOfTag );
+        if( endOfTag == -1 )
+            endOfTag = content.length();
+        return content.substring( startOfTag + tag.length(), endOfTag );
+    }
+
+    private void deserialiseDescription() {
+        int startOfDescription = content.indexOf( locationDescriptionTag );
+        if( startOfDescription != -1 )
+            location.setLocationDescription( content.substring(
+                        startOfDescription + locationDescriptionTag.length(),
+                        findEndOfDescription() ) );
+    }
+
+    private int findEndOfDescription() {
+        int endOfDescription = content.indexOf( exitLabelTag );
+        if( endOfDescription == -1 ) {
+            endOfDescription = content.indexOf( itemTag );
+            if( endOfDescription == -1 )
+                endOfDescription = content.length();
+        }
+        return endOfDescription;
+    }
+
+    private void deserialiseExits() {
+        String exitLabel;
+        while( (exitLabel=extractNewlineDelimitedValueFor( exitLabelTag )) != "" ) {
+            LocationExit exit =  new LocationExit(
+                    exitLabel,
+                    extractNewlineDelimitedValueFor( exitDestinationTag ),
+                    stringToDirectionHint(
+                        extractNewlineDelimitedValueFor( exitDirectionHintTag ) ) );
+
+            if( exitNotVisibleIsSpecifiedDiscardIt() )
+                exit.setInvisible();
+            exit.setID( extractExitID() );
+            location.addExit( exit );
+        }
+    }
+
+    private boolean exitNotVisibleIsSpecifiedDiscardIt() {
+        int startOfTag = content.indexOf( exitIsNotVisibleTag, startOfLastFoundTag + 1 );
+        if( indexIsInCurrentExit( startOfTag ) ) {
+            extractNewlineDelimitedValueFor( exitIsNotVisibleTag );
+            return true;
+        }
+        return false;
+    }
+
+    private String extractExitID() {
+        int startOfTag = content.indexOf( exitIDTag, startOfLastFoundTag + 1 );
+        if( indexIsInCurrentExit( startOfTag ) )
+            return extractNewlineDelimitedValueFor( exitIDTag );
+        return "";
+    }
+
+    private boolean indexIsInCurrentExit( int index ) {
+        int nextExit = content.indexOf( exitLabelTag, startOfLastFoundTag + 1 );
+        if( index != -1 && ( nextExit == -1 || nextExit > index ) )
+            return true;
+        return false;
+    }
+
+    private void deserialiseItems() {
+        if( itemFactory != null ) {
+            while( itemAvailable() ) {
+                int startOfItem = content.indexOf( itemTag, startOfLastFoundTag+1 );
+                int endOfItem = content.indexOf( itemTag, startOfItem+1 );
+                if( endOfItem == -1 ) {
+                    endOfItem = content.indexOf( locationIDTag, startOfItem+1 );
+                    if( endOfItem == -1 )
+                        endOfItem = content.length();
+                }
+                Item item = itemFactory.create();
+                item.deserialise(
+                        content.substring( startOfItem + itemTag.length(), endOfItem ) );
+                location.addItem( item );
+                startOfLastFoundTag = startOfItem;
+            }
+        }
+    }
+
+    private boolean itemAvailable() {
+        return content.indexOf( itemTag, startOfLastFoundTag+1 ) != -1;
+    }
+
+    private Exit.DirectionHint stringToDirectionHint( String hint ) {
+        if( hint.equals( "North" ) )
+            return Exit.DirectionHint.North;
+        if( hint.equals( "South" ) )
+            return Exit.DirectionHint.South;
+        if( hint.equals( "East" ) )
+            return Exit.DirectionHint.East;
+        if( hint.equals( "West" ) )
+            return Exit.DirectionHint.West;
+        return Exit.DirectionHint.DontCare;
+    }
+}
+
