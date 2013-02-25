03c36da14b7398dd9b775ef41efd2fdb05c1b909
Julian Churchill <chewielouie03@gmail.com>
2013-02-25 21:14:09 +0000

    Added commented tests for moving Exit serialisation out of ModelLocationDeserialiser

--- AndroidFree/src/test/java/com/chewielouie/textadventure/serialisation/ModelLocationDeserialiserTests.java
+++ AndroidFree/src/test/java/com/chewielouie/textadventure/serialisation/ModelLocationDeserialiserTests.java
@@ -157,6 +157,93 @@ public class ModelLocationDeserialiserTests {
         //assertEquals( Exit.DirectionHint.North, l.visibleExits().get(1).directionHint() );
     //}
 
+
+
+    // New tests...
+
+    //@Test
+    //public void deserialise_extracts_exit() {
+        //final ModelLocation location = mockery.mock( ModelLocation.class );
+        //final Exit exit = mockery.mock( Exit.class );
+        //final ExitFactory exitFactory = mockery.mock( ExitFactory.class );
+        //ModelLocationDeserialiser d =
+            //new ModelLocationDeserialiser( location, null, exitFactory );
+
+        //mockery.checking( new Expectations() {{
+            //allowing( exitFactory ).create();
+            //will( returnValue( exit ) );
+            //ignoring( exitFactory );
+            //oneOf( exit ).deserialise( "exit 1\n" +
+                                       //"some more content" );
+            //ignoring( exit );
+            //ignoring( location );
+        //}});
+
+        //d.deserialise( "location id:name\n" +
+                       //"EXIT\nexit 1\n" +
+                       //"some more content" );
+    //}
+
+    //@Test
+    //public void deserialise_extracts_multiple_exits() {
+        //final ModelLocation location = mockery.mock( ModelLocation.class );
+        //final Exit exit1 = mockery.mock( Exit.class, "exit1" );
+        //final Exit exit2 = mockery.mock( Exit.class, "exit2" );
+        //final ExitFactory exitFactory = mockery.mock( ExitFactory.class );
+        //ModelLocationDeserialiser d =
+            //new ModelLocationDeserialiser( location, null, exitFactory );
+
+        //mockery.checking( new Expectations() {{
+            //atLeast( 1 ).of( exitFactory ).create();
+                //will( onConsecutiveCalls(
+                      //returnValue( exit1 ),
+                      //returnValue( exit2 ) ) );
+            //ignoring( exitFactory );
+            //oneOf( exit1 ).deserialise( "exit 1 content\n" +
+                                       //"some more content" );
+            //ignoring( exit1 );
+            //oneOf( exit2 ).deserialise( "exit 2 content\n" +
+                                       //"some more content" );
+            //ignoring( exit2 );
+            //ignoring( location );
+        //}});
+
+        //d.deserialise( "location id:name\n" +
+                       //"EXIT\nexit 1 content\n" +
+                       //"some more content" +
+                       //"EXIT\nexit 2 content\n" +
+                       //"some more content" );
+    //}
+
+    //@Test
+    //public void deserialise_adds_extracted_exits_to_location() {
+        //final ModelLocation location = mockery.mock( ModelLocation.class );
+        //final Exit exit1 = mockery.mock( Exit.class, "exit1" );
+        //final Exit exit2 = mockery.mock( Exit.class, "exit2" );
+        //final ExitFactory exitFactory = mockery.mock( ExitFactory.class );
+        //ModelLocationDeserialiser d =
+            //new ModelLocationDeserialiser( location, null, exitFactory );
+
+        //mockery.checking( new Expectations() {{
+            //atLeast( 1 ).of( exitFactory ).create();
+                //will( onConsecutiveCalls(
+                      //returnValue( exit1 ),
+                      //returnValue( exit2 ) ) );
+            //ignoring( exitFactory );
+            //ignoring( exit1 );
+            //ignoring( exit2 );
+            //oneOf( location ).addExit( exit1 );
+            //oneOf( location ).addExit( exit2 );
+            //ignoring( location );
+        //}});
+
+        //d.deserialise( "location id:name\n" +
+                       //"EXIT\nexit 1 content\n" +
+                       //"and more exit content\n" +
+                       //"EXIT\nexit 2 content\n" +
+                       //"and more exit content\n" );
+    //}
+
     @Test
     public void deserialise_extracts_item() {
         final ModelLocation location = mockery.mock( ModelLocation.class );
--- README.md
+++ README.md
@@ -9,7 +9,8 @@ Backlog
 =======
 
 - [REFACTOR] Exits should deserialise themselves - not Location
-   - Deserialiser needs to be initialized with a LocationExitFactory
+   - LocationExit needs to deserialise itself
+   - Deserialiser should call LocationExit.deserialise
    - Deserialiser should use the LocationExitFactory
 - [REFACTOR] Deserialisation should be pulled out into seperate classes - ModelDeserialiser, ModelLocationDeserialiser, ItemDeserialiser - all coordinate with a PlainTextDeserialiser used by the PlainTextModelPopulator
 - [REFACTOR] Consider making the order of statements in the model text irrelevant. Currently the deserialisation depends on a strict ordering - this might be a pain and lead to silly errors in the model text that are not obvious to find.
