8ed0f10dcd5874c9a4f22b37ab90c23175ffa5c0
Julian Churchill <chewielouie03@gmail.com>
2013-05-12 20:18:18 +0100

    Adde deserialisation of multiple action history lines

--- AndroidFree/src/main/java/com/chewielouie/textadventure/serialisation/ActionHistoryDeserialiser.java
+++ AndroidFree/src/main/java/com/chewielouie/textadventure/serialisation/ActionHistoryDeserialiser.java
@@ -1,29 +1,46 @@
 package com.chewielouie.textadventure.serialisation;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import com.chewielouie.textadventure.TextAdventureModel;
 import com.chewielouie.textadventure.UserInventory;
+import com.chewielouie.textadventure.action.Action;
 import com.chewielouie.textadventure.action.ActionFactory;
-import com.chewielouie.textadventure.action.ActionHistory;
 
 public class ActionHistoryDeserialiser {
-    private ActionHistory history;
     private ActionFactory factory;
     private UserInventory inventory;
     private TextAdventureModel model;
 
-    public ActionHistoryDeserialiser( ActionHistory history,
-                                      ActionFactory factory,
+    public ActionHistoryDeserialiser( ActionFactory factory,
                                       UserInventory inventory,
                                       TextAdventureModel model ) {
-        this.history = history;
         this.factory = factory;
         this.inventory = inventory;
         this.model = model;
     }
 
-    public void deserialise( String input ) {
-        history.clear();
-        if( factory != null )
-            factory.createShowInventoryAction( inventory, model );
+    public List<Action> deserialise( String input ) {
+        List<Action> actions = new ArrayList<Action>();
+        if( factory != null ) {
+            Scanner scanner = new Scanner( input );
+            while( scanner.hasNextLine() )
+                extractActionFromLine( scanner.nextLine(), actions );
+        }
+        return actions;
+    }
+
+    private void extractActionFromLine( String input, List<Action> actions ) {
+        int current = input.indexOf( ":" );
+        if( current != -1 ) {
+            int startOfActionName = current + 1;
+            int endOfActionName = input.indexOf( ":", startOfActionName );
+            if( endOfActionName != -1 ) {
+                String actionName = input.substring( startOfActionName,
+                                                     endOfActionName );
+                actions.add( factory.createShowInventoryAction( inventory, model ) );
+            }
+        }
     }
 }
--- AndroidFree/src/test/java/com/chewielouie/textadventure/serialisation/ActionHistoryDeserialiserTests.java
+++ AndroidFree/src/test/java/com/chewielouie/textadventure/serialisation/ActionHistoryDeserialiserTests.java
@@ -1,46 +1,58 @@
 package com.chewielouie.textadventure.serialisation;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-// import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.*;
 
+import org.mockito.Mockito;
 import org.junit.Test;
+import java.util.List;
 import com.chewielouie.textadventure.TextAdventureModel;
 import com.chewielouie.textadventure.UserInventory;
 import com.chewielouie.textadventure.action.Action;
 import com.chewielouie.textadventure.action.ActionFactory;
-import com.chewielouie.textadventure.action.ActionHistory;
 import com.chewielouie.textadventure.action.ActionParameters;
 
 public class ActionHistoryDeserialiserTests {
 
     @Test
-    public void clears_history_if_nothing_to_deserialise() {
-        ActionHistory history = mock( ActionHistory.class );
-        new ActionHistoryDeserialiser( history, null, null, null ).deserialise( "" );
-
-        verify( history ).clear();
-        verify( history, never() ).addActionWithParameters( any( Action.class ),
-                                                            any( ActionParameters.class ) );
+    public void returns_empty_list_of_actions_for_empty_input() {
+        List<Action> actions =
+            new ActionHistoryDeserialiser( null, null, null ).deserialise( "" );
+        assertThat( actions.size(), is( 0 ) );
     }
 
     @Test
     public void deserialises_ShowInventory_action_type_using_factory() {
         ActionFactory factory = mock( ActionFactory.class );
-        ActionHistory history = mock( ActionHistory.class );
         UserInventory inventory = mock( UserInventory.class );
         TextAdventureModel model = mock( TextAdventureModel.class );
-        new ActionHistoryDeserialiser( history, factory, inventory, model ).deserialise(
+        new ActionHistoryDeserialiser( factory, inventory, model ).deserialise(
             "action name:show inventory:\n" );
 
         verify( factory ).createShowInventoryAction( inventory, model );
     }
 
     // @Test
-    // public void adds_deserialised_action_to_history() {
-
-    // @Test
     // public void deserialises_xxx_action_type() {
+
+    @Test
+    public void adds_deserialised_actions_to_list_in_order() {
+        Action action1 = mock( Action.class );
+        Action action2 = mock( Action.class );
+        ActionFactory factory = mock( ActionFactory.class );
+        when( factory.createShowInventoryAction( Mockito.any( UserInventory.class ),
+                                                 Mockito.any( TextAdventureModel.class ) ) )
+            .thenReturn( action1, action2 );
+        List<Action> actions =
+            new ActionHistoryDeserialiser( factory, null, null ).deserialise(
+            "action name:show inventory:\n" +
+            "action name:show inventory:\n" );
+
+        assertThat( actions.size(), is( 2 ) );
+        assertThat( actions.get( 0 ), is( action1 ) );
+        assertThat( actions.get( 1 ), is( action2 ) );
+    }
 
     // @Test
     // public void deserialises_action_item_parameter() {
