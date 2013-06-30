ce82bcc66dafb428d8936f25b3fae3ad07d8d227
Julian Churchill <chewielouie03@gmail.com>
2013-06-30 17:07:01 +0100

    Score is incremented when necklace is found

--- AndroidFree/src/main/java/com/chewielouie/textadventure/BasicModel.java
+++ AndroidFree/src/main/java/com/chewielouie/textadventure/BasicModel.java
@@ -13,7 +13,7 @@ public class BasicModel implements TextAdventureModel, UserInventory {
     ModelLocation currentLocation = new NullLocation();
     private List<Item> inventoryItems = new ArrayList<Item>();
     private int currentScore = 0;
-    private int maximumScore = 2;
+    private int maximumScore = 3;
     private List<ModelEventSubscriber> eventSubscribers =
                                     new ArrayList<ModelEventSubscriber>();
 
