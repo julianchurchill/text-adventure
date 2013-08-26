8da3626cd2b79361911add47ab12ec35ce5dd0e0
Julian Churchill <chewielouie03@gmail.com>
2013-08-26 19:13:35 +0100

    Added config for app 2

--- config/prepare.sh
+++ config/prepare.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-cp config/1/AndroidManifest.xml .
-cp -R config/1/res .
+function usage()
+{
+    echo "Usage: prepare.sh <app id>"
+    echo " where app id can currently be one of [1 2]"
+}
+
+if [ -z "$1" ]
+then
+    usage
+    exit 1
+fi
+
+case "$1" in
+    1)  echo 1
+        cp config/1/AndroidManifest.xml .
+        cp -R config/1/res .
+        ;;
+    2)  echo 2
+        cp config/2/AndroidManifest.xml .
+        cp -R config/2/res .
+        ;;
+    *)  usage
+        ;;
+esac
+
