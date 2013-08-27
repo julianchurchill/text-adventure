815ad4574f09e5be721961e63e33c6fa93275dff
Julian Churchill <chewielouie03@gmail.com>
2013-08-27 12:31:28 +0100

    Specific activity now extends common activity and provides resource access methods

--- src/main/java/com/chewielouie/textadventure/TextAdventureActivity.java
+++ src/main/java/com/chewielouie/textadventure/TextAdventureActivity.java
@@ -21,5 +21,109 @@ public class TextAdventureActivity extends TextAdventureCommonActivity {
     public TextAdventureActivity( UserActionHandler u ) {
         super( u );
     }
-}
 
+    protected int R_drawable_map() {
+        return R.drawable.map;
+    }
+
+    protected int R_id_available_actions() {
+        return R.id.available_actions;
+    }
+
+    protected int R_id_location_text_view() {
+        return R.id.location_text_view;
+    }
+
+    protected int R_id_map_view() {
+        return R.id.map_view;
+    }
+
+    protected int R_id_main_text_output() {
+        return R.id.main_text_output;
+    }
+
+    protected int R_id_main_text_output_scroll_view() {
+        return R.id.main_text_output_scroll_view;
+    }
+
+    protected int R_id_options_font_example_text() {
+        return R.id.options_font_example_text;
+    }
+
+    protected int R_id_options_font_size_picker() {
+        return R.id.options_font_size_picker;
+    }
+
+    protected int R_id_score_text_view() {
+        return R.id.score_text_view;
+    }
+
+    protected int R_layout_about_dialog() {
+        return R.layout.about_dialog;
+    }
+
+    protected int R_layout_main() {
+        return R.layout.main;
+    }
+
+    protected int R_layout_options_dialog() {
+        return R.layout.options_dialog;
+    }
+
+    protected int R_raw_model_content() {
+        return R.raw.model_content;
+    }
+
+    protected int R_string_about() {
+        return R.string.about;
+    }
+
+    protected int R_string_app_name() {
+        return R.string.app_name;
+    }
+
+    protected int R_string_completed() {
+        return R.string.completed;
+    }
+
+    protected int R_string_new_game() {
+        return R.string.new_game;
+    }
+
+    protected int R_string_new_game_title() {
+        return R.string.new_game_title;
+    }
+
+    protected int R_string_new_game_confirmation_dialog_text() {
+        return R.string.new_game_confirmation_dialog_text;
+    }
+
+    protected int R_string_no() {
+        return R.string.no;
+    }
+
+    protected int R_string_options() {
+        return R.string.options;
+    }
+
+    protected int R_string_options_title() {
+        return R.string.options_title;
+    }
+
+    protected int R_string_options_save() {
+        return R.string.options_save;
+    }
+
+    protected int R_string_options_cancel() {
+        return R.string.options_cancel;
+    }
+
+    protected int R_string_show_map() {
+        return R.string.show_map;
+    }
+
+    protected int R_string_yes() {
+        return R.string.yes;
+    }
+
+}
--- src/main/java/com/chewielouie/textadventure/TextAdventureCommonActivity.java
+++ src/main/java/com/chewielouie/textadventure/TextAdventureCommonActivity.java
@@ -79,7 +79,34 @@ import com.chewielouie.textadventure.itemaction.LoggableNormalItemActionFactory;
 import com.chewielouie.textadventure_common.BasicModelV1_0ToActionListConverter;
 import com.chewielouie.textadventure_common.JSONToActionListConverter;
 
-public class TextAdventureCommonActivity extends Activity implements TextAdventureView, OnClickListener {
+public abstract class TextAdventureCommonActivity extends Activity implements TextAdventureView, OnClickListener {
+
+    abstract protected int R_drawable_map();
+    abstract protected int R_id_available_actions();
+    abstract protected int R_id_location_text_view();
+    abstract protected int R_id_map_view();
+    abstract protected int R_id_main_text_output();
+    abstract protected int R_id_main_text_output_scroll_view();
+    abstract protected int R_id_options_font_example_text();
+    abstract protected int R_id_options_font_size_picker();
+    abstract protected int R_id_score_text_view();
+    abstract protected int R_layout_about_dialog();
+    abstract protected int R_layout_main();
+    abstract protected int R_layout_options_dialog();
+    abstract protected int R_raw_model_content();
+    abstract protected int R_string_about();
+    abstract protected int R_string_app_name();
+    abstract protected int R_string_completed();
+    abstract protected int R_string_new_game();
+    abstract protected int R_string_new_game_title();
+    abstract protected int R_string_new_game_confirmation_dialog_text();
+    abstract protected int R_string_no();
+    abstract protected int R_string_options();
+    abstract protected int R_string_options_title();
+    abstract protected int R_string_options_save();
+    abstract protected int R_string_options_cancel();
+    abstract protected int R_string_show_map();
+    abstract protected int R_string_yes();
 
     private static final int ABOUT_MENU_ITEM = 0;
     private static final int NEW_GAME_MENU_ITEM = 1;
@@ -158,110 +185,6 @@ public class TextAdventureCommonActivity extends Activity implements TextAdventu
         score_text_view = findTextView( R_id_score_text_view() );
         available_actions_view = (LinearLayout)findViewById( R_id_available_actions() );
         map_view = (ImageView)findViewById( R_id_map_view() );
-    }
-
-    protected int R_drawable_map() {
-        return R.drawable.map;
-    }
-
-    protected int R_id_available_actions() {
-        return R.id.available_actions;
-    }
-
-    protected int R_id_location_text_view() {
-        return R.id.location_text_view;
-    }
-
-    protected int R_id_map_view() {
-        return R.id.map_view;
-    }
-
-    protected int R_id_main_text_output() {
-        return R.id.main_text_output;
-    }
-
-    protected int R_id_main_text_output_scroll_view() {
-        return R.id.main_text_output_scroll_view;
-    }
-
-    protected int R_id_options_font_example_text() {
-        return R.id.options_font_example_text;
-    }
-
-    protected int R_id_options_font_size_picker() {
-        return R.id.options_font_size_picker;
-    }
-
-    protected int R_id_score_text_view() {
-        return R.id.score_text_view;
-    }
-
-    protected int R_layout_about_dialog() {
-        return R.layout.about_dialog;
-    }
-
-    protected int R_layout_main() {
-        return R.layout.main;
-    }
-
-    protected int R_layout_options_dialog() {
-        return R.layout.options_dialog;
-    }
-
-    protected int R_raw_model_content() {
-        return R.raw.model_content;
-    }
-
-    protected int R_string_about() {
-        return R.string.about;
-    }
-
-    protected int R_string_app_name() {
-        return R.string.app_name;
-    }
-
-    protected int R_string_completed() {
-        return R.string.completed;
-    }
-
-    protected int R_string_new_game() {
-        return R.string.new_game;
-    }
-
-    protected int R_string_new_game_title() {
-        return R.string.new_game_title;
-    }
-
-    protected int R_string_new_game_confirmation_dialog_text() {
-        return R.string.new_game_confirmation_dialog_text;
-    }
-
-    protected int R_string_no() {
-        return R.string.no;
-    }
-
-    protected int R_string_options() {
-        return R.string.options;
-    }
-
-    protected int R_string_options_title() {
-        return R.string.options_title;
-    }
-
-    protected int R_string_options_save() {
-        return R.string.options_save;
-    }
-
-    protected int R_string_options_cancel() {
-        return R.string.options_cancel;
-    }
-
-    protected int R_string_show_map() {
-        return R.string.show_map;
-    }
-
-    protected int R_string_yes() {
-        return R.string.yes;
     }
 
     private class LoadTask extends AsyncTask<Void, Void, Void> {
