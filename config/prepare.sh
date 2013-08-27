7155376457cbe0c62e85e50f6f7abdcf20dc1c54
Julian Churchill <chewielouie03@gmail.com>
2013-08-27 23:14:11 +0100

    Tidied up prepare.sh a bit

--- config/prepare.sh
+++ config/prepare.sh
@@ -11,12 +11,19 @@ function removeOldFiles()
     rm -r AndroidManifest.xml res
 }
 
+function removeOldSourceFilesForOtherPackage()
+{
+    packageName=$1
+    rm -r src/main/java/com/chewielouie/$packageName
+    rm -r src/test/java/com/chewielouie/$packageName
+}
+
 function copyCommonFiles()
 {
     cp -R config/common/res .
 }
 
-function copyTestFiles()
+function copyTestFilesForPackage()
 {
     packageName=$1
     mkdir -p src/test/java/com/chewielouie/$packageName
@@ -32,6 +39,14 @@ function monkeyPatchPackageNamesForTestFiles()
     done
 }
 
+function copyFilesFromConfigArea()
+{
+    configArea=$1
+    cp config/$configArea/AndroidManifest.xml .
+    cp -R config/$configArea/res .
+    cp -R config/$configArea/src .
+}
+
 if [ -z "$1" ]
 then
     usage
@@ -41,23 +56,17 @@ fi
 case "$1" in
     1)  echo "Configuring app id 1"
         removeOldFiles
-        rm -r src/main/java/com/chewielouie/textadventure2
-        rm -r src/test/java/com/chewielouie/textadventure2
+        removeOldSourceFilesForOtherPackage textadventure2
         copyCommonFiles
-        copyTestFiles textadventure
-        cp config/1/AndroidManifest.xml .
-        cp -R config/1/res .
-        cp -R config/1/src .
+        copyTestFilesForPackage textadventure
+        copyFilesFromConfigArea 1
         ;;
     2)  echo "Configuring app id 2"
         removeOldFiles
-        rm -r src/main/java/com/chewielouie/textadventure
-        rm -r src/test/java/com/chewielouie/textadventure
+        removeOldSourceFilesForOtherPackage textadventure
         copyCommonFiles
-        copyTestFiles textadventure2
-        cp config/2/AndroidManifest.xml .
-        cp -R config/2/res .
-        cp -R config/2/src .
+        copyTestFilesForPackage textadventure2
+        copyFilesFromConfigArea 2
         ;;
     *)  usage
         ;;
