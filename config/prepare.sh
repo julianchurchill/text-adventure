22006200aba32c53fa2205f532e38fbe1562a9a4
Julian Churchill <chewielouie03@gmail.com>
2014-06-18 21:44:04 +0100

    Fixed prepare.sh so that it properly replaces files when switching configurations

--- config/prepare.sh
+++ config/prepare.sh
@@ -18,6 +18,13 @@ function removeOldSourceFilesForOtherPackage()
     rm --force --recursive src/test/java/com/chewielouie/$packageName
 }
 
+function createNewPackage()
+{
+    packageName=$1
+    mkdir --parents src/main/java/com/chewielouie/$packageName
+    mkdir --parents src/test/java/com/chewielouie/$packageName
+}
+
 function copy()
 {
     src=$1
@@ -25,9 +32,28 @@ function copy()
     cp --update --recursive $src $dest
 }
 
+function copyNewAndDifferingFilesFromSrcDirectoryToDestination()
+{
+    src=$1
+    dest=$2
+    echo "Copying directory contents from $src to $dest"
+    cd $src && fileList=`find . -type f -print` && cd - > /dev/null
+    for file in $fileList; do
+        fileSrc=$src/$file
+        fileDest=$dest/$file
+        if [ "$fileSrc" -nt "$fileDest" ] ; then
+            echo " copying newer     $fileSrc"
+            cp $fileSrc $fileDest
+        elif ! cmp $fileSrc $fileDest >& /dev/null ; then
+            echo " copying differing $fileSrc"
+            cp $fileSrc $fileDest
+        fi
+    done
+}
+
 function copyCommonFiles()
 {
-    copy config/common/res .
+    copyNewAndDifferingFilesFromSrcDirectoryToDestination config/common/res res
 }
 
 function copyTestFilesForPackage()
@@ -35,7 +61,6 @@ function copyTestFilesForPackage()
     packageName=$1
     testDirectoryRoot=src/test/java/com/chewielouie
     testDirectory=$testDirectoryRoot/$packageName
-    mkdir --parents $testDirectory
     copy "config/common/test/*" $testDirectory
     monkeyPatchPackageNamesForTestFiles $packageName $testDirectoryRoot
 }
@@ -53,8 +78,8 @@ function copyFilesFromConfigArea()
 {
     configArea=$1
     copy config/$configArea/AndroidManifest.xml .
-    copy config/$configArea/res .
-    copy config/$configArea/src .
+    copyNewAndDifferingFilesFromSrcDirectoryToDestination config/$configArea/res res
+    copyNewAndDifferingFilesFromSrcDirectoryToDestination config/$configArea/src src
 }
 
 # remove all files in res that are not in config/$configArea/res or config/common/res
@@ -83,6 +108,7 @@ case "$1" in
     1)  echo "Configuring app id 1"
         removeOldManifest
         removeOldSourceFilesForOtherPackage textadventure2
+        createNewPackage textadventure
         copyCommonFiles
         copyTestFilesForPackage textadventure
         copyFilesFromConfigArea 1
@@ -91,6 +117,7 @@ case "$1" in
     2)  echo "Configuring app id 2"
         removeOldManifest
         removeOldSourceFilesForOtherPackage textadventure
+        createNewPackage textadventure2
         copyCommonFiles
         copyTestFilesForPackage textadventure2
         copyFilesFromConfigArea 2
